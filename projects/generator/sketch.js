ebdf283eb910426bcbf886041223b2587ae188fa
alexafreyes <alexafreyes@users.noreply.github.com>
2020-04-15 00:15:03 -0400

    generator

--- projects/generator/sketch.js
+++ projects/generator/sketch.js
@@ -24,7 +24,7 @@ function setup() {
 }
 
 function draw() {
-  background(255, 50); //add opacity/trail
+  background(255, 30); //add opacity/trail
 
 
   //call the functions of the object
@@ -52,38 +52,34 @@ class Ball {
     this.yDirection = 1;
     this.d = val;
    this.col =c;
-    
-    // this.xAcc = random(0.51, 0.5);
-    // this.xAngle = random(0, 60);
-    // this.yAcc = random(0.01, 0.5);
-    // this.yAngle = random(0, 60);
+
   }
   //how its displayed on the canvas
   display() {
-    // fill(colorPicker.color());
-    //noStroke();
+
+   
     strokeWeight(0);
     fill(this.col);
-    ellipse(this.x, this.y, this.d);
+    rectMode(CENTER);
+    // beginShape();
+    // square(this.x, (this.y-(this.d/2)), (this.d/2),);
+    triangle((this.x-(this.d/2)),this.y,this.x,(this.y-(this.d+5)),(this.x+(this.d/2)),this.y)
+    arc(this.x, this.y, this.d,this.d,0,180,);
+    
+    // endShape(CLOSE);
+    
   }
 
   //movement
   move() {
     this.y = (this.y + this.yDirection);
-    // this.x = this.x + (this.xDirection*this.xAcc*cos(this.xAngle));
-    // this.y = this.y + (this.yDirection*this.yAcc*sin(this.yAngle));
-    // if (this.y > height)
-    //   //reset value of x position
-    //   this.y = -40;
-    // this.x = random(width)
+    
   }
 
   intersect(x, y) {
     let d = dist(this.x, this.y, x, y);
     let r = dist(this.x - this.d / 2, this.y, x, y);
     let l = dist(this.x + this.d / 2, this.y, x, y);
-    // let t = dist(this.x, this.y + this.d / 2, x, y);
-    // let b = dist(this.x, this.y - this.d / 2, x, y);
 
     if (l > this.d / 2 && d < this.d / 2) {
       print("left");
@@ -93,13 +89,6 @@ class Ball {
       this.x--;
     }
 
-    // if (b > this.d / 2 && d < this.d / 2) {
-    //   print("bottom");
-    //   this.y++;
-    // } else if (t > this.d / 2 && d < this.d / 2) {
-    //   print("top");
-    //   this.y--;
-    // }
 
   }
 
